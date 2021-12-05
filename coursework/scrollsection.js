f570097fe675698c05bde307c04f0c76bdf74a2e
Maxim Nesterov <76449260+Fyrestyrter@users.noreply.github.com>
2021-12-05 16:16:22 +0300

    coursework
    123123

---
+++ coursework/scrollsection.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function(){
+    jQuery('nav a[href^="#"]' ).click( function(){
+        var scroll_el = jQuery(this).attr('href');
+        var destination = jQuery(scroll_el).offset().top;
+        if (jQuery(scroll_el).length != 0) {
+            jQuery('html, body').animate( { scrollTop: destination }, 600 );
+        }
+        return false;
+    });
+})
